27776f13adf314617d699937742bd97e56a394f5
Lukepw23 <116756110+Lukepw23@users.noreply.github.com>
2022-10-26 14:57:35 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// ------------------------------ Variables ------------------------------ //
+
+projectDropdownState = false;
+
+const PROJECT_DROPDOWN_BTN = document.getElementById("project-dropdown");
+const PROJECT_DROPDOWN = document.getElementById("project-dropdown-menu");
+
+// --------------------------- Event Listeners --------------------------- //
+
+PROJECT_DROPDOWN_BTN.addEventListener("click", switch_project_dropdown);
+
+
+// ------------------------------ Functions ------------------------------ //
+
+function switch_project_dropdown() {
+
+    if (projectDropdownState == false) {
+
+        PROJECT_DROPDOWN.style.height = "300px";
+        projectDropdownState = true;
+
+    }
+    else if (projectDropdownState == true) {
+
+        PROJECT_DROPDOWN.style.height = "0px";
+        projectDropdownState = false;
+
+    }
+
+}
